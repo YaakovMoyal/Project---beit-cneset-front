0a5e643f342ccf00c041d21f5b344117c471874f
YaakovMoyal <kobi856m@gmail.com>
2024-01-24 16:58:12 +0200

    edit bc

--- src/features/beitCneset/pages/EditBeitCnesetPage.tsx
+++ src/features/beitCneset/pages/EditBeitCnesetPage.tsx
@@ -1,12 +1,19 @@
 import { useParams } from "react-router-dom";
 import { MyDiv } from "../../global/style/MyDiv.styled";
+import { jwtDecode } from "jwt-decode";
+import { TokenIF } from "../../users/interfaces/tokenIF";
 
 export default function EditBeitCnesetPage() {
   const { bc } = useParams();
+  const token = localStorage.getItem("token");
+  if (!token) return "please login first";
+
+  const decodedToken: TokenIF = jwtDecode(token);
 
   return (
     <MyDiv>
       in Edit Beit Cneset Page
+      <div>{decodedToken._id}</div>
       <div>{bc}</div>
     </MyDiv>
   );
